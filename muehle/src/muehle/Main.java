2bc9aefba7edac7ff1a4bcbbafd24d4e6efeb527
Japeth18 <yafitandemicael9@gmail.com>
2024-08-15 13:15:04 +0200

    yafet

--- muehle/src/muehle/Main.java
+++ muehle/src/muehle/Main.java
@@ -1,19 +1,33 @@
 package muehle;
+import spieler.Spieler;
+
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		// test Stein Klasse von Hatem
-		Stein S1 = new Stein ("S1",0,0,'w');
-		Stein S2 = new Stein ("S1",0,1,'w');
-		Stein S3 = new Stein ("S1",0,2,'w');
-		
-		S1.getSteinPosition();
-		S2.getSteinPosition();
-		S3.getSteinPosition();
-		System.err.println("HI");	
-	}
-	
-	
+    public static void main(String[] args) {
+        // Erstelle einen neuen Spieler mit Namen und Farbe
+        Spieler spieler1 = new Spieler("Spieler 1", 'r');
 
+        // Setze einige Steine für den Spieler
+        spieler1.steinSetzen("Stein1", 0, 0);
+        spieler1.steinSetzen("Stein2", 1, 1);
+        spieler1.steinSetzen("Stein3", 2, 2);
+
+        // Zeige die Positionen der gesetzten Steine an
+        System.out.println("Positionen der Steine nach dem Setzen:");
+        List<Stein> steine = spieler1.getSteine();
+        for (Stein stein : steine) {
+            System.out.println("Stein: " + stein.name + " Position: x=" + stein.steinX + ", y=" + stein.steinY);
+        }
+
+        // Ziehe einen Stein zu einer neuen Position
+        spieler1.steinZiehen("Stein1", 3, 3);
+
+        // Zeige die Positionen der Steine nach dem Ziehen an
+        System.out.println("\nPositionen der Steine nach dem Ziehen:");
+        for (Stein stein : steine) {
+            System.out.println("Stein: " + stein.name + " Position: x=" + stein.steinX + ", y=" + stein.steinY);
+        }
+    }
 }
+
