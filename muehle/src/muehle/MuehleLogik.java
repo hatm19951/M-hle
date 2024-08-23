05e27fa8f6c8180e9332412abd72a3e483fa83df
Kim_Kevin <122615332+kimkevin-cloud@users.noreply.github.com>
2024-08-23 12:39:11 +0200

    Logik hinzugefügt

---
+++ muehle/src/muehle/MuehleLogik.java
@@ -0,0 +1,17 @@
+package muehle
+
+import java.awt.*;
+
+public class MuehleLogik {
+
+
+    public MuehleLogik() {
+    }
+
+
+    public void handleClick(int x, int y) {
+    }
+
+    public void drawGame(Graphics2D g2d) {
+    }
+}
